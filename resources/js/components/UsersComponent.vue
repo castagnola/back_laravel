b0e9902a8a9375ee49cf6e0ac2e54ac3b106f705
fabio <castagnola92@gmail.com>
2019-11-22 09:45:19 -0500

    New User Components vue and modal

---
+++ resources/js/components/UsersComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div style="margin-top: 10px" class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">User List</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-block btn-outline-success" data-toggle="modal" data-target="#addUser">Add new
+                            <i class="fas fa-user-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Status</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>183</td>
+                                <td>John Doe</td>
+                                <td>11-7-2014</td>
+                                <td><span class="tag tag-success">Approved</span></td>
+                                <td>
+                                    <button type="button" class="btn bg-gradient-primary btn-sm"> <i class="fa fa-edit"></i></button>
+                                    <button type="button" class="btn bg-gradient-danger btn-sm">  <i class="fa fa-trash"></i></button>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal -->
+        <user-modal-component></user-modal-component>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
